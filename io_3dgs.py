92ed33dead15ecdde26daba271ca2247f8395c04
syjintw <yuanjiun911@outlook.com>
2026-01-23 22:41:01 +0800

    Update io_3dgs.py

--- io_3dgs.py
+++ io_3dgs.py
@@ -2,232 +2,16 @@ import numpy as np
 from plyfile import PlyData, PlyElement
 
 """
-Load different SH of 3DGS
-Export 3DGS
-    - Different SH degree
-    - Binary / ASCII-version of 3DGS
-    - Only export specific attributes
-Reduce SH degree
-Delete Gaussian points
-Get boundary
-Shift 3DGS
+Tools for handling Gaussian models stored in PLY files
+- load_gaussian_ply()
+- export_gs_to_ply()
+- extract_gaussians()
+- copy_gs()
+- add_attribute()
+- delete_attribute()
+- concatenate_gs()
+- get_AABB()
 """
-
-class GaussianModel():
-    num_of_point = 0
-    sh_deg = -1
-    xyz = -1
-    features_dc = -1
-    features_rest = -1
-    opacities = -1
-    scales = -1
-    rots = -1
-
-    def __init__(self, gs_path: str):
-        self.load_gaussian_ply(gs_path)
-        pass
-    
-    def __str__(self):
-        return f"This Gaussians contains {self.num_of_point} of Gaussian point and SH degree is {self.sh_deg}"
-        
-    def load_gaussian_ply(self, path: str) -> list:
-        
-        with open(path, 'rb') as f:
-            plydata = PlyData.read(f)
-        
-        # --------------------------------- Position --------------------------------- #
-        self.xyz = np.stack((np.asarray(plydata.elements[0]["x"]),
-                            np.asarray(plydata.elements[0]["y"]),
-                            np.asarray(plydata.elements[0]["z"])),  axis=1)
-        
-        # ------------------------- Number of Gaussian points ------------------------ #
-        self.num_of_point = self.xyz.shape[0]
-                
-        # ------------------------------------ SH0 ----------------------------------- #
-        self.features_dc = np.zeros((self.num_of_point, 3, 1))
-        self.features_dc[:, 0, 0] = np.asarray(plydata.elements[0]["f_dc_0"])
-        self.features_dc[:, 1, 0] = np.asarray(plydata.elements[0]["f_dc_1"])
-        self.features_dc[:, 2, 0] = np.asarray(plydata.elements[0]["f_dc_2"])
-
-        # -------------------------- Additional degree of SH ------------------------- #
-        #! [YC] Modify it to support lower SH
-        extra_f_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("f_rest_")]
-        extra_f_names = sorted(extra_f_names, key = lambda x: int(x.split('_')[-1]))
-        # assert len(extra_f_names)==3*(3 + 1) ** 2 - 3
-        self.sh_deg = int(((len(extra_f_names) + 3) / 3) ** (1/2) - 1) # Calculate SH degree
-        if self.sh_deg != 0:
-            self.features_rest = np.zeros((self.num_of_point, len(extra_f_names)))
-            for idx, attr_name in enumerate(extra_f_names):
-                self.features_rest[:, idx] = np.asarray(plydata.elements[0][attr_name])
-            # Reshape (P, F*SH_coeffs) to (P, F, SH_coeffs except DC)
-            self.features_rest = self.features_rest.reshape((self.features_rest.shape[0], 3, (self.sh_deg + 1) ** 2 - 1))
-
-        # ---------------------------------- Opacity --------------------------------- #
-        self.opacities = np.asarray(plydata.elements[0]["opacity"])[..., np.newaxis]
-
-        # ----------------------------------- Scale ---------------------------------- #
-        scale_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("scale_")]
-        scale_names = sorted(scale_names, key = lambda x: int(x.split('_')[-1]))
-        self.scales = np.zeros((self.num_of_point, len(scale_names)))
-        for idx, attr_name in enumerate(scale_names):
-            self.scales[:, idx] = np.asarray(plydata.elements[0][attr_name])
-
-        # --------------------------------- Rotation --------------------------------- #
-        rot_names = [p.name for p in plydata.elements[0].properties if p.name.startswith("rot")]
-        rot_names = sorted(rot_names, key = lambda x: int(x.split('_')[-1]))
-        self.rots = np.zeros((self.num_of_point, len(rot_names)))
-        for idx, attr_name in enumerate(rot_names):
-            self.rots[:, idx] = np.asarray(plydata.elements[0][attr_name])
-
-    def export_gs_to_ply(self, save_path: str, 
-                         xyz=True, normals=True, colors=True, opacities=True, scales=True, rots=True,
-                         ascii=False):
-        
-        # -------------------------- Create attribute names -------------------------- #
-        l = []
-        data = tuple()
-        if xyz:
-            l.extend(['x', 'y', 'z'])
-            data += (self.xyz,)
-
-        if normals:
-            l.extend(["nx", "ny", "nz"])
-            _normals = np.zeros_like(self.xyz) # Create fake normal data
-            data += (_normals,)
-            
-        # All channels except the 3 DC
-        if colors:
-            for i in range(self.features_dc.shape[1]*self.features_dc.shape[2]):
-                l.append('f_dc_{}'.format(i))
-            _f_dc = self.features_dc.reshape(self.features_dc.shape[0], self.features_dc.shape[1]*self.features_dc.shape[2])
-            data += (_f_dc,)
-
-            if self.features_rest.shape[0] > 0: # Handle SH0
-                for i in range(self.features_rest.shape[1]*self.features_rest.shape[2]):
-                    l.append('f_rest_{}'.format(i))
-                _f_rest = self.features_rest.reshape(self.features_rest.shape[0], self.features_rest.shape[1]*self.features_rest.shape[2])
-                data += (_f_rest,)
-
-        if opacities:
-            l.append('opacity')
-            data += (self.opacities,)
-
-        if scales:
-            for i in range(self.scales.shape[1]):
-                l.append('scale_{}'.format(i))
-            data += (self.scales,)
-
-        if rots:
-            for i in range(self.rots.shape[1]):
-                l.append('rot_{}'.format(i))
-            data += (self.rots,)
-        
-        # ---------------- Using PlyElement library to store Gaussian ---------------- #
-        dtype_full = [(attribute, 'f4') for attribute in l]
-        elements = np.empty(self.num_of_point, dtype=dtype_full)
-        attributes = np.concatenate(data, axis=1)
-        elements[:] = list(map(tuple, attributes))
-        el = PlyElement.describe(elements, 'vertex')
-        
-        if ascii:
-            PlyData([el], text=True).write(save_path)
-        else:
-            PlyData([el]).write(save_path)
-    
-    def shift(self, center=[0,0,0]):
-        self.xyz = self.xyz + center
-    
-    def get_bound(self):
-        max_indices = []
-        max_values = []
-        for i in range(3):
-            _col = self.xyz[:, i]
-            _max_index = np.argmax(_col)
-            _max_value = _col[_max_index]
-            max_indices.append(_max_index)
-            max_values.append(_max_value)
-
-        min_indices = []
-        min_values = []
-        for i in range(3):
-            _col = self.xyz[:, i]
-            _min_index = np.argmin(_col)
-            _min_value = _col[_min_index]
-            min_indices.append(_min_index)
-            min_values.append(_min_value)
-
-        return min_values, max_values, min_indices, max_indices
-
-    def delete_Gaussian(self, indices_to_delete: list):
-        if self.xyz is not -1:
-            self.xyz = np.delete(self.xyz, indices_to_delete, axis=0)
-        if self.opacities is not -1:
-            self.opacities = np.delete(self.opacities, indices_to_delete, axis=0)
-        if self.features_dc is not -1:
-            self.features_dc = np.delete(self.features_dc, indices_to_delete, axis=0)
-        if self.features_rest is not -1:
-            self.features_rest = np.delete(self.features_rest, indices_to_delete, axis=0)
-        if self.scales is not -1:
-            self.scales = np.delete(self.scales, indices_to_delete, axis=0)
-        if self.rots is not -1:
-            self.rots = np.delete(self.rots, indices_to_delete, axis=0)
-        
-        # ---------------------------- Update num_of_point --------------------------- #
-        self.num_of_point = self.xyz.shape[0]
-
-    def reduce_SH(self, target_sh_deg):
-        if target_sh_deg < self.sh_deg:
-            num_of_coefficient = 3 * (target_sh_deg + 1) ** 2 - 3
-            self.features_rest = self.features_rest[:,:,:int(num_of_coefficient/3)]
-            self.sh_deg = target_sh_deg
-        else:
-            print(f"The target degree ({target_sh_deg}) is larger than Gaussians' degree ({self.sh_deg})")
-
-    def limit_x(self, _min, _max):
-        _bound = self.get_bound()
-        _min = max(_min, _bound[0][0])
-        _max = min(_max, _bound[1][0])
-        print(np.where((self.xyz[:, 0] < _min) | (self.xyz[:, 0] > _max)))
-        indices = np.where((self.xyz[:, 0] < _min) | (self.xyz[:, 0] > _max))[0]
-        print(f"Delete {indices.shape[0]} points")
-        self.delete_Gaussian(indices)
-    
-    def limit_y(self, _min, _max):
-        _bound = self.get_bound()
-        _min = max(_min, _bound[0][1])
-        _max = min(_max, _bound[1][1])
-        print(_min, _max)
-        indices = np.where((self.xyz[:, 1] < _min) | (self.xyz[:, 1] > _max))[0]
-        print(f"Delete {indices.shape[0]} points")
-        self.delete_Gaussian(indices)
-
-    def limit_z(self, _min, _max):
-        _bound = self.get_bound()
-        _min = max(_min, _bound[0][2])
-        _max = min(_max, _bound[1][2])
-        print(_min, _max)
-        indices = np.where((self.xyz[:, 2] < _min) | (self.xyz[:, 2] > _max))[0]
-        print(f"Delete {indices.shape[0]} points")
-        self.delete_Gaussian(indices)
-
-    def recenter(self):
-        bound = self.get_bound()
-        center_x = (bound[0][0] + bound[1][0]) / 2
-        center_y = (bound[0][1] + bound[1][1]) / 2
-        center_z = (bound[0][2] + bound[1][2]) / 2
-        self.shift([0-center_x, 0-center_y, 0-center_z])
-    
-    def sort(self):
-        # sort_idx = np.argsort(self.xyz[:, 0])
-        sort_idx = np.argsort(self.opacities[:, 0])
-        print(sort_idx)
-        self.xyz = self.xyz[sort_idx]
-        self.features_dc = self.features_dc[sort_idx]
-        self.features_rest = self.features_rest[sort_idx]
-        self.opacities = self.opacities[sort_idx]
-        self.scales = self.scales[sort_idx]
-        self.rots = self.rots[sort_idx]
-
 
 class GaussianModelV2():
     """
@@ -240,7 +24,7 @@ class GaussianModelV2():
     def __init__(self, gs_path: str):
         self.load_gaussian_ply(gs_path)
 
-    def copy(self):
+    def copy_gs(self):
         new_gs = GaussianModelV2.__new__(GaussianModelV2)
         new_gs.data = self.data.copy()
         new_gs.num_of_point = self.num_of_point
@@ -312,3 +96,45 @@ class GaussianModelV2():
             PlyData([el], text=True).write(save_path)
         else:
             PlyData([el]).write(save_path)
+
+    def concatenate_gs(self, other_gs):
+        """
+        Concatenates another GaussianModelV2 instance to the current one.
+        
+        [POS_s][COLOR_o]...[ROT_o]
+                   +
+        [POS_o][COLOR_o]...[ROT_o]
+        """
+        
+        if not isinstance(other_gs, GaussianModelV2):
+            raise TypeError("other_gs must be an instance of GaussianModelV2")
+        
+        new_num_of_points = self.num_of_point + other_gs.num_of_point
+
+        # Check if keys match
+        if set(self.data.keys()) != set(other_gs.data.keys()):
+            raise ValueError("The two Gaussian models must have the same attributes to concatenate.")
+        
+        for key in self.data.keys():
+            self.data[key]["data"] = np.concatenate((self.data[key]["data"], other_gs.data[key]["data"]), axis=0)
+            if self.data[key]["data"].shape[0] != new_num_of_points:
+                raise ValueError(f"Concatenation failed for attribute {key}. Expected {new_num_of_points} points, got {self.data[key]['data'].shape[0]}")
+        
+        self.num_of_point = new_num_of_points
+        
+    def get_AABB(self):
+        """
+        Compute the Axis-Aligned Bounding Box (AABB) of the Gaussian model.
+        Returns min_bound and max_bound as numpy arrays.
+        """
+        if 'x' not in self.data or 'y' not in self.data or 'z' not in self.data:
+            raise ValueError("Position attributes 'x', 'y', 'z' are required to compute AABB.")
+        
+        x = self.data['x']['data']
+        y = self.data['y']['data']
+        z = self.data['z']['data']
+        
+        min_bound = np.array([np.min(x), np.min(y), np.min(z)])
+        max_bound = np.array([np.max(x), np.max(y), np.max(z)])
+        
+        return min_bound, max_bound
